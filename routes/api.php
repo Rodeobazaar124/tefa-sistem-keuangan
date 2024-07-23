e685ffb032f10045c6accc9e8e4cc56f87a432e3
kharlinptr <kharlinarfahwidyakp@gmail.com>
2024-07-23 13:50:16 +0700

    Penambahan CRUD inventaris asset
    Co-authored-by: meliaaa <AmeliaaAja@users.noreply.github.com>
    Co-authored-by: Muhammad Azfa Salman Akbar <Rodeobazaar124@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\AssetController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- * route "/register"
- * @method "POST"
- */
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 
-/**
- * route "/login"
- * @method "POST"
- */
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 
-/**
- * route "/user"
- * @method "GET"
- */
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-/**
- * route "/logout"
- * @method "POST"
- */
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->post('/logout', LogoutController::class)->name('logout');
+Route::middleware('auth:api')->apiResource('/assets', AssetController::class);

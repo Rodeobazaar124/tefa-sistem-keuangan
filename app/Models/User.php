71727e7393d201833b39418c3a868366b60093a9
kharlinptr <kharlinarfahwidyakp@gmail.com>
2024-07-26 11:24:20 +0700

    mengubah fitur aset beserta modelnya
    Co-authored-by: meliaaa <AmeliaaAja@users.noreply.github.com>
    Co-authored-by: Muhammad Azfa Salman Akbar <Rodeobazaar124@users.noreply.github.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,24 +3,31 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
+protected$table ='users';
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**

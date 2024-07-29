105147e0d10524f7363cc131802b09f3ba6643a4
kharlinptr <kharlinarfahwidyakp@gmail.com>
2024-07-29 09:07:03 +0700

    membuat fitur print
    Co-authored-by: meliaaa <AmeliaaAja@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\Api\AssetController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\PrintInventaris;
 use App\Models\Asset;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('inventaris',PrintInventaris::class);
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
